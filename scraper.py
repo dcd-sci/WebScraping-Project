b1a3d31c87b4a7c3d034805ebfd3cd544f2c73f7
Davi C. <dcdsci.db@gmail.com>
2025-03-24 15:19:54 -0300

    fixing typos

--- scraper.py
+++ scraper.py
@@ -29,8 +29,8 @@ while True:
 
     print(products_dict)
 
-    if n == 14:
-        print("Scraping complete.") # this number is hard coded which is not ideal but it'll do for the moment
+    if n == 14: # this number is hard coded which is not ideal but it'll do for the moment
+        print("Scraping complete.")
         break
 
     base_url = f'https://www.pakatatu.com/categoria-produto/loja/page/{n}/'
